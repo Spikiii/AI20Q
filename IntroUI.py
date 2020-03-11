37b27739072a70b86fb59c072c614805ca71c908
Eddie Xu <50087436+EddieXu123@users.noreply.github.com>
2020-03-11 15:53:22 -0400

    Create IntroUI.py
    Template page for the game when the user plays

---
+++ IntroUI.py
@@ -0,0 +1,10 @@
+def ui_screen():
+    input1 = input("Please enter your category: 'Animal', 'Plant', Mineral'\n")
+    if input1 == "Animal":
+        # connect to Animal branch
+    if input1 == "Plant":
+        # connect to Plant branch
+    if input1 == "Mineral":
+        # connect to Mineral branch
+
+ui_screen()
