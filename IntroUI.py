dcb9cd374a1530ad476b2d6a1a9b8a27930c2c8f
Bea Shakow <beashakow@gmail.com>
2020-03-24 19:22:21 -0400

    Update IntroUI.py
    Added Mode selection to UI

--- IntroUI.py
+++ IntroUI.py
@@ -1,10 +1,14 @@
 def ui_screen():
-    input1 = input("Please enter your category: 'Animal', 'Plant', Mineral'\n")
-    if input1 == "Animal":
-        # connect to Animal branch
-    if input1 == "Plant":
-        # connect to Plant branch
-    if input1 == "Mineral":
-        # connect to Mineral branch
+    mode = input("What mode do you want to use? 'Game' or 'Data'\n"); #figure out which user class this user is in
+    if mode == "Game":
+        input1 = input("Please enter your category: 'Animal', 'Plant', Mineral'\n") #determine
+        if input1 == "Animal":
+            # connect to Animal branch
+        if input1 == "Plant":
+            # connect to Plant branch
+        if input1 == "Mineral":
+            # connect to Mineral branch
+    if mode == "Data":
+        print("This feature isn't implemented yet, but it will be in the future.")
 
 ui_screen()
