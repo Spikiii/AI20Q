cd0cfca40cf85675e8c2174fc6c1001f1f4e5dda
Bea Shakow <beashakow@gmail.com>
2020-03-24 21:52:05 -0400

    Update gameState.py
    Fixed names in gameState to be less confusing

--- gameState.py
+++ gameState.py
@@ -1,24 +1,24 @@
 import dataNode
 
 class gameState:
-    chars = []
-    objects = []
+    charsKnown = []
+    objectsGuessed = []
     category = ""
 
     def __init__(self, cat = ""):
         self.category = cat
 
-    def getChars(self):#return the charsGuessed
-        return self.chars
+    def getChars(self):#return the chars known
+        return self.charsKnown
 
-    def getObjs(self):#returns the list of objects
-        return self.objects
+    def getObjs(self):#returns the list of objects guessed so far
+        return self.objectsGuessed
 
     def getCategory(self):#gets the category of the game
         return self.category
 
     def addChar(self, char):#adds a characteristic to the list
-        chars.append(char)
+        charsKnown.append(char)
 
-    def addObj(self, object):#adds an object to the list of 
-        objects.append(object)
+    def addObj(self, object):#adds an object to the list of
+        objectsGuessed.append(object)
