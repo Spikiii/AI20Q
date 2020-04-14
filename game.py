7604f676f53f5a6ddef852d3eedcea60534bec4c
Bea Shakow <beashakow@gmail.com>
2020-04-14 13:41:07 -0400

    Added Eytan's Code

--- game.py
+++ game.py
@@ -13,7 +13,8 @@ dataBase = []
 #Settings
 charDictPath = "data/charDict.txt"
 dataPath = "data/data.txt"
-confidenceGuess = 0.8 #Controls the confidence that the AI is willing to guess at
+confidenceGuess = 0.5 #Controls the confidence that the AI is willing to guess at
+minQuestions = 10 #Minimum number of questions to wait before guessing
 
 #Dictionaries
 charDict = {} #Characteristic dictionary as {characteristic:(id,qtype)}
@@ -70,15 +71,22 @@ def saveData(cPD = charDictPath, dP = dataPath):
 
     #Creating charDict file
     charLines = []
+    tags = []
     charLines.append("#Character Dictionary")
     charLines.append("#ID,char,qtype")
     charLines.append("")
 
     #Converts everything in qDict to a string format for export
-    count = 0
-    for i in charDict.keys():
-        charLines.append(str(count) + "," + i + "," + str(charDict[i][1]))
-        count += 1
+    id = 0
+    for i in dataBase:
+        for j in i.getTags():
+            if(j.get() not in tags):
+                tags.append(j.get())
+                try:
+                    charLines.append(str(id) + "," + j.get() + "," + str(charDict[j.get()][1]))
+                except:
+                    charLines.append(str(id) + "," + j.get() + "," + "-1")
+                id += 1
 
     #Write charLines into cPD
     with open(cPD, "w") as f:
@@ -152,12 +160,208 @@ def mainMenu():
         else:
             print("Please enter a valid mode.")
             mode = str.lower(input("> "))
-            print("")
 
 def dataMode():
     """Data exploration mode"""
-    print("test!")
+    modeSelecting = True
+    mode = ""
 
+    print("Please select a mode: 'Search by [o]bject', 'Search by [c]haracteristic', 'Search by [s]imilarity'.")
+    input1 = str.lower(input("> "))
+    while(modeSelecting):
+        if input1 == "search by object" or input1 == "object" or input1 == "o":
+            modeSelecting = False
+            mode = "o"
+        elif input1 == "search by characteristic" or input1 == "characteristic" or input1 == "c":
+            modeSelecting = False
+            mode = "c"
+        elif input1 == "search by similarity" or input1 == "similarity" or input1 == "s":
+            modeSelecting = False
+            mode = "s"
+        elif input1 == "quit" or input1 == "q":
+            return
+        else:
+            print("Please enter a valid mode.")
+            input1 = str.lower(input("> "))
+
+    objSearching = True
+    while(objSearching):
+        obj = ("",0)
+        if(mode == "o"):
+            print("Please enter the object you would like to search for:")
+            input2 = str.lower(input("> "))
+            for i in dataBase:
+                if(i.get() == input2):
+                    obj = (input2,dataBase.index(i))
+            if(obj == ("",0)):
+                print("It looks like your object was not found in the database.")
+                getObjFeedBack(input2)
+
+            else:
+                print("[" + str(len(dataBase[obj[1]].getTags())) + "] characteristics associated with [" + obj[0] + "]:")
+                for i in dataBase[obj[1]].getTags():
+                    print(" " + i.get())
+
+            entrySelecting = True
+
+            print("Would you like to search for another object?[y/n]")
+            input1 = input("> ")
+
+            while(entrySelecting):
+                if (input1 == "yes" or input1 == "y"):
+                    entrySelecting = False
+                    objSearching = True
+                elif (input1 == "no" or input1 == "n" or input1 == "quit" or input1 == "q"):
+                    return
+                else:
+                    print("Please enter [y] or [n].")
+                    input1 = str.lower(input("> "))
+
+        elif(mode == "s"):
+            print("Please enter the object you would like to search for:")
+            input2 = str.lower(input("> "))
+            for i in dataBase:
+                if(i.get() == input2):
+                    obj = (input2,dataBase.index(i))
+            if(obj == ("",0)):
+                print("It looks like your object was not found in the database.")
+                getObjFeedBack(input2)
+            else:
+                #Calculating all similarities
+                dataBase[obj[1]].calcSim(dataBase)
+                sims = dataBase[obj[1]].getSims()
+                for i in list(sims.keys()):
+                    if(sims[i] < confidenceGuess):
+                        sims.pop(i)
+
+                sims.pop(obj[0]) #Making sure that obj is not in the list
+
+                print("[" + str(len(sims)) + "] objects with high similarity associated with [" + obj[0] + "]:")
+                if(len(sims) == 0):
+                    print("")
+                else:
+                    for i in sims:
+                        print(" " + i)
+                    print("")
+
+            entrySelecting = True
+
+            print("Would you like to search for another object?[y/n]")
+            input1 = input("> ")
+
+            while(entrySelecting):
+                if (input1 == "yes" or input1 == "y"):
+                    entrySelecting = False
+                    objSearching = True
+                elif (input1 == "no" or input1 == "n" or input1 == "quit" or input1 == "q"):
+                    return
+                else:
+                    print("Please enter [y] or [n].")
+                    input1 = str.lower(input("> "))
+
+        elif(mode == "c"):
+            print("Please enter the characteristic you would like to search for:")
+            input2 = str.lower(input("> "))
+            for i in dataBase:
+                for j in i.getTags():
+                    if (input2 == j.get()):
+                        obj = (input2, dataBase.index(i))
+            if(obj == ("", 0)):
+                print("It looks like that characteristic was not found in the database.")
+            else:
+                charCount = 0
+                objs = []
+                for i in dataBase:
+                    charFound = False
+                    for j in i.getTags():
+                        if(obj[0] == j.get() and j.getTruth()):
+                            charCount += 1
+                            charFound = True
+                    if(charFound):
+                        objs.append(i)
+
+                print("[" + str(len(objs)) + "] objects with the characteristic [" + obj[0] + "]:")
+                for i in objs:
+                    print(" " + i.get())
+
+            entrySelecting = True
+
+            print("Would you like to search for another characteristic?[y/n]")
+            input1 = input("> ")
+
+            while(entrySelecting):
+                if (input1 == "yes" or input1 == "y"):
+                    entrySelecting = False
+                    objSearching = True
+                elif (input1 == "no" or input1 == "n" or input1 == "quit" or input1 == "q"):
+                    return
+                else:
+                    print("Please enter [y] or [n].")
+                    input1 = str.lower(input("> "))
+
+        else:
+            print("::Error in mode selection::")
+
+def getObjFeedBack(obj):
+    """Gets a response from the user to whether or not they would like to create an entry, and then to enter some characteristics."""
+
+    entrySelecting = True
+
+    print("Would you like to create an entry for [" + obj + "]? [y/n]")
+    input1 = input("> ")
+
+    while(entrySelecting):
+        if(input1 == "yes" or input1 == "y"):
+            entrySelecting = False
+        elif(input1 == "no" or input1 == "n" or input1 == "quit" or input1 == "q"):
+            return
+        else:
+            print("Please enter [y] or [n].")
+            input1 = str.lower(input("> "))
+
+    categorySelecting = True
+    cat = ""
+
+    print("Please enter the category of [" + obj + "]: '[A]nimal', '[P]lant', '[M]ineral', or '[O]ther'.")
+    input2 = str.lower(input("> "))
+    while (categorySelecting):
+        if input2 == "animal" or input2 == "a":
+            categorySelecting = False
+            cat = "animal"
+        elif input2 == "plant" or input2 == "p":
+            categorySelecting = False
+            cat = "plant"
+        elif input2 == "mineral" or input2 == "m":
+            categorySelecting = False
+            cat = "mineral"
+        elif input2 == "other" or input2 == "o":
+            categorySelecting = False
+            cat = "other"
+        elif input2 == "quit" or input2 == "q":
+            return
+        else:
+            print("Please enter a valid category.")
+            input2 = str.lower(input("> "))
+
+    print("Please enter 3 - 5 characteristics of [" + obj + "].")
+    print("If you would like to negate a characteristic, put a ! before the characteristic.")
+    print("For example, if your object is [mouse], !wings means that a mouse does not have wings.")
+    print("If you do not want to enter a characteristic, just press enter.")
+
+    charsRaw = []
+    chars = []
+
+    for i in range(1, 6):
+        charsRaw.append(input(str(i) + ". "))
+    for i in charsRaw:
+        if(i != "" and i[0] == "!"):
+            chars.append(Char.characteristic(i[1:], False, -1))
+        elif(i != "" and i[0] != "!"):
+            chars.append(Char.characteristic(i, True, -1))
+    dataBase.append(Dn.dataNode(obj, cat, chars))
+    saveData()
+
+    print("Entry created.")
 
 def play20Q():
     """Plays the game!"""
@@ -179,11 +383,10 @@ def play20Q():
             categorySelecting = False
             game.setCategory("other")
         elif input1 == "quit" or input1 == "q":
-            return "EXIT"
+            return
         else:
             print("Please enter a valid category.")
             input1 = str.lower(input("> "))
-            print("")
 
     questions = 0  # keeps track of number of questions asked
     playing = True  # keeps track of whether the player is still playing
@@ -191,12 +394,15 @@ def play20Q():
     while (playing and questions <= 19):
         questions += 1  # increment questions
         guess = oG.guessObject(game)
+        print(guess)
 
-        if(guess[1] >= confidenceGuess or questions == 20):
+        if((guess[1] >= confidenceGuess and questions >= minQuestions and guess[0] not in game.getObjs()) or questions == 20):
             print(str(questions) + ". I think it is a [" + guess[0] + "]")
             print("Is this correct? [y/n]")
             ans = str.lower(input("> "))
             entering = True
+            game.addObj(guess[0])
+
             while(entering):
                 if(ans == "y" or ans == "yes"):
                     playing = False
